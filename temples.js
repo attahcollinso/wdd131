1b511bd00b877bbead88dee01231c1a38a47e089
attahcollinso <attahcollinso@gmail.com>
2024-09-30 14:50:52 +0100

    desc and author

--- temples.js
+++ temples.js
@@ -1,42 +1,35 @@
-//  function myFunction() {
-//     var x = document.getElementById("myTopnav");
-//     if (x.className === "topnav") {
-//       x.className += " responsive";
-//     } else {
-//       x.className = "topnav";
-//     }
-//   }
+ function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
 
-// function toggleMobileMenu(menu) {
-//     menu.classList.toggle("open");
-//     }
+function toggleMobileMenu(menu) {
+    menu.classList.toggle("open");
+    }
+const $btnBurguer = document.querySelector("#btnBurguer");
+const $navBar = document.querySelector("#navBar");
 
-// const d = new Date();
-// let year = d.getFullYear();
-// document.getElementById("currentyear").innerHTML = year;
+$btnBurguer.addEventListener("click", () => {
+  $navBar.classList.toggle("display");
+  if ($navBar.classList.contains("display")) {
+    $btnBurguer.textContent = "⛌";
+  } else {
+    $btnBurguer.textContent = "☰";
+  }
+});
 
-// let text = document.lastModified;
-// document.getElementById("lastModified").innerHTML = text;
-// note
-// const $year = document.querySelector("#currentyear");
-// const $lastModified = document.querySelector("#lastModified");
-// const $btnBurguer = document.querySelector("#btnBurguer");
-// const $navBar = document.querySelector("#navBar");
-
-// const today = new Date();
-
-// $year.innerHTML = today.getFullYear();
-// $lastModified.innerHTML = "Last Modification: ${document.lastModified}";
-
-// $btnBurguer.addEventListener("click", () => {
-//   $navBar.classList.toggle("display");
-//   if ($navBar.classList.contains("display")) {
-//     $btnBurguer.textContent = "⛌";
-//   } else {
-//     $btnBurguer.textContent = "☰";
-//   }
-// });
-
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
 
 const d = new Date();
 let year = d.getFullYear();
