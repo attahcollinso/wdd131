1665c6c10b1d11703bc142646eb600a6c571db96
attahcollinso <attahcollinso@gmail.com>
2024-10-05 10:47:24 +0100

    week03 place assignment

---
+++ scripts/place.js
@@ -0,0 +1,56 @@
+
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("currentyear").innerHTML = year;
+
+let text = document.lastModified;
+document.getElementById("lastModified").innerHTML = text;
+
+const temperature = 26.9;
+const windSpeed = 9.5;
+
+function calculateWindChill(temp, wind) {
+  return (
+    35.74 +
+    0.6215 * temp -
+    35.75 * Math.pow(wind, 0.16) +
+    0.4275 * temp * Math.pow(wind, 0.16)
+  );
+}
+
+function displayWindChill() {
+  const weatherSection = document.getElementById("weather");
+  let windChill = "N/A";
+
+  if (temperature <= 50 && windSpeed > 3) {
+    windChill = calculateWindChill(temperature, windSpeed).toFixed(2);
+  }
+
+  weatherSection.textContent = `Wind Chill: ${windChill} °F`;
+}
+
+const temperatureElement = document.getElementById("temperature");
+temperatureElement.textContent = `${temperature} °F`;
+
+const conditionsElement = document.getElementById("conditions");
+conditionsElement.textContent = "Partly Cloudy";
+
+const windElement = document.getElementById("wind");
+windElement.textContent = `${windSpeed} km/h`;
+
+const windChillElement = document.getElementById("windChill");
+windChillElement.textContent = "9.8°C";
+
+
+const date = new Date();
+
+function getCurrentYear(date) {
+  return date.getFullYear();
+}
+
+function getLastModified(date) {
+  return `${new Intl.DateTimeFormat("en-US", {
+    dateStyle: "short",
+  }).format(date)}`;
+}
+
